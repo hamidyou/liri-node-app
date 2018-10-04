6fc287f20fcb18a591ba3c5c225c8796e0104fe8
Youssef <yhamid1982@gmail.com>
2018-10-04 08:07:17 -0400

    spotify api working

--- liri.js
+++ liri.js
@@ -1,12 +1,41 @@
 require('dotenv').config()
 
 const keys = require('./keys')
+const Spotify = require('node-spotify-api')
+const request = require('request')
+const k = require('kyanite/dist/kyanite')
 
 const spotify = new Spotify(keys.spotify)
 
-const concert = 'concert-this'
-const spot = 'spotify-this-song'
-const movie = 'movie-this'
-const doWhatItSays = 'do-what-it-says'
+// const concert = 'concert-this'
+// const spot = 'spotify-this-song'
+// const movie = 'movie-this'
+// const doWhatItSays = 'do-what-it-says'
 
-const bitAPIquery = 'https://rest.bandsintown.com/artists/' + artist + '/events?app_id=codingbootcamp'
+// const bitAPIquery = 'https://rest.bandsintown.com/artists/' + artist + '/events?app_id=codingbootcamp'
+
+if (process.argv[2] === 'spotify-this-song') {
+  spotify.search({ type: 'track', query: k.join('', k.slice(3, process.argv)), limit: 50 }, function (err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err)
+    }
+
+    const trackInfoObj = k.pipe([
+      k.sift(x => x.preview_url),
+      k.draft(k.props(['popularity', 'name', 'album', 'artists', 'preview_url']))
+    ], data.tracks.items)
+
+    const trackInfo = k.pipe([
+      k.values,
+      k.sortBy(x => x[0]),
+      k.last
+    ], trackInfoObj)
+
+    const artist = trackInfo[3][0].name
+    const song = trackInfo[2].name
+    const preview = trackInfo[4]
+    const album = trackInfo[1]
+
+    console.log('Artist: ' + artist + '\n' + 'Song Title: ' + song + '\n' + 'Preview Link: ' + preview + '\n' + 'Album: ' + album)
+  })
+}
---
+++ test.js
@@ -0,0 +1,88 @@
+{
+  "tracks": {
+    "href": "https://api.spotify.com/v1/search?query=Motown+Philly&type=track&market=US&offset=0&limit=1",
+      "items": [
+        {
+          "album": {
+            "album_type": "single",
+            "artists": [
+              {
+                "external_urls": {
+                  "spotify": "https://open.spotify.com/artist/13EahEu09Px92S8GLirDey"
+                },
+                "href": "https://api.spotify.com/v1/artists/13EahEu09Px92S8GLirDey",
+                "id": "13EahEu09Px92S8GLirDey",
+                "name": "7th Ave",
+                "type": "artist",
+                "uri": "spotify:artist:13EahEu09Px92S8GLirDey"
+              }
+            ],
+            "external_urls": {
+              "spotify": "https://open.spotify.com/album/2g4rIvcqAu8I17TpTFa7JQ"
+            },
+            "href": "https://api.spotify.com/v1/albums/2g4rIvcqAu8I17TpTFa7JQ",
+            "id": "2g4rIvcqAu8I17TpTFa7JQ",
+            "images": [
+              {
+                "height": 640,
+                "url": "https://i.scdn.co/image/023827516a3086dc31bb5aadb9fae11605f3eddc",
+                "width": 640
+              },
+              {
+                "height": 300,
+                "url": "https://i.scdn.co/image/2cbeb05d5f09d8742464818250700f53253fbd05",
+                "width": 300
+              },
+              {
+                "height": 64,
+                "url": "https://i.scdn.co/image/0735668ba818c4420154b1edca5f985e9b742dec",
+                "width": 64
+              }
+            ],
+            "name": "Millennial Medley: I Want It That Way / Wannabe / It's Gonna Be Me / No Scrubs / I Do (Cherish You)/Don't Cha / Motown Philly / Survivor",
+            "release_date": "2016-09-29",
+            "release_date_precision": "day",
+            "total_tracks": 1,
+            "type": "album",
+            "uri": "spotify:album:2g4rIvcqAu8I17TpTFa7JQ"
+          },
+          "artists": [
+            {
+              "external_urls": {
+                "spotify": "https://open.spotify.com/artist/13EahEu09Px92S8GLirDey"
+              },
+              "href": "https://api.spotify.com/v1/artists/13EahEu09Px92S8GLirDey",
+              "id": "13EahEu09Px92S8GLirDey",
+              "name": "7th Ave",
+              "type": "artist",
+              "uri": "spotify:artist:13EahEu09Px92S8GLirDey"
+            }
+          ],
+          "disc_number": 1,
+          "duration_ms": 254878,
+          "explicit": false,
+          "external_ids": {
+            "isrc": "ushm81622212"
+          },
+          "external_urls": {
+            "spotify": "https://open.spotify.com/track/0SFsJz1mcFe7V111VuKMfi"
+          },
+          "href": "https://api.spotify.com/v1/tracks/0SFsJz1mcFe7V111VuKMfi",
+          "id": "0SFsJz1mcFe7V111VuKMfi",
+          "is_local": false,
+          "is_playable": true,
+          "name": "Millennial Medley: I Want It That Way / Wannabe / It's Gonna Be Me / No Scrubs / I Do (Cherish You)/ Don't Cha / Motown Philly / Survivor",
+          "popularity": 16,
+          "preview_url": "https://p.scdn.co/mp3-preview/012fef0f20d3cae53bcf0606936d3026c4934b13?cid=774b29d4f13844c495f206cafdad9c86",
+          "track_number": 1,
+          "type": "track",
+          "uri": "spotify:track:0SFsJz1mcFe7V111VuKMfi"
+        }
+      ],
+        "limit": 1,
+          "next": "https://api.spotify.com/v1/search?query=Motown+Philly&type=track&market=US&offset=1&limit=1",
+            "offset": 0,
+              "previous": null,
+                "total": 25
+  }
+}
